0cbfb5664f19e4878aa84ef8b4d6b4ac2f2b9dd3
ArKxdy <the.maniaxofficial@gmail.com>
2024-01-21 22:14:46 +0530

    Password Generator

---
+++ passwordgenerator/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PasswordGenerator from './PasswordGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordGenerator></PasswordGenerator>
+    </div>
+  );
+}
+
+export default App;
